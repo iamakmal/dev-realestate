0a11082e6bfd3c9cc5e3b59fe9dded5e60aabf1a
iamakmal <m.akmalalavi@gmail.com>
2024-04-26 11:10:41 +0530

    implemented navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="navbar bg-transparent">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </div>
+      <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+            <a>Home</a>
+        </li>
+        <li>
+          <a>Luxury Villa</a>
+        </li>
+        <li>
+            <a>Properties</a>
+        </li>
+        <li>
+            <a>Blog</a>
+        </li>
+        <li>
+            <a>About Us</a>
+        </li>
+        <li>
+            <a>Contacts</a>
+        </li>
+      </ul>
+    </div>
+    <div className="w-1/4">
+    <a className="btn btn-ghost text-2xl text-[#0C7F40] ml-2 -mb-3">R-STATE</a>
+    <p className="text-xs text-center">Real Estate</p>
+    </div>
+  </div>
+  <div className="navbar-start hidden lg:flex w-3/4 mr-16">
+    <ul className="menu menu-horizontal px-1 group text-lg">
+    <li>
+        <a className="hover:text-main hover:bg-transparent hover:underline hover:underline-offset-8">Home</a>
+    </li>
+    <li>
+        <a className="hover:text-main hover:bg-transparent hover:underline hover:underline-offset-8">Luxury Villa</a>
+    </li>
+    <li>
+        <a className="hover:text-main hover:bg-transparent hover:underline hover:underline-offset-8">Properties</a>
+    </li>
+    <li>
+        <a className="hover:text-main hover:bg-transparent hover:underline hover:underline-offset-8">Blog</a>
+    </li>
+    <li>
+        <a className="hover:text-main hover:bg-transparent hover:underline hover:underline-offset-8">About Us</a>
+    </li>
+    <li>
+        <a className="hover:text-main hover:bg-transparent hover:underline hover:underline-offset-8">Contacts</a>
+    </li>
+    </ul>
+  </div>
+</div>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+      main:'#078169',
+      sub:'#6F6C90',
+      yellow:'#F7BF00',
+      black:'#171717',
+      white:'#FFFFFF',
+      red:'#FF7C46',
+      orange:"#F28D35"
+    },      
+    fontFamily: {
+      prata: ['Prata', 'serif'],
+      poppins: ['Poppins', 'sans-serif'],
+    },
   },
-  plugins: [],
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "cupcake"],
+  },
 }

36ff091c7d0c8205b5fbf1c1f48ff239b989c747
iamakmal <m.akmalalavi@gmail.com>
2024-04-26 14:03:24 +0530

    implemented header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 
 function App() {
   
   return (
     <>
+    <div className="relative">
+    <Hero/>
+    <div className="absolute inset-x-0 top-0 z-10">
     <Navbar/>
+    </div>
+    </div>
     </>
   )
 }

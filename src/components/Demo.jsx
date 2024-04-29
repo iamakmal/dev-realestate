374b2248d90f58877c98fd01a5b9518f9f77f47c
it21292422 <it21292422@my.sliit.lk>
2024-04-29 08:54:47 +0530

    added avatar to demo component

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -32,12 +32,21 @@ export default function Demo() {
 
        </div>
 
-       <div className="grid grid-cols-2 bg-main text-white h-20">
+       <div className="grid grid-cols-2 bg-main text-white h-20 mt-10">
         <div className="flex justify-center items-center ">
             <p className="text-2xl font-prata">Gorgeous Home For Sale In Greenville</p>
         </div>
         <div className="flex justify-center items-center">
-            <button className="btn btn-outline w-50 text-white">
+            <div className="flex mx-10">
+            <div>
+            <img src="demoavatar.png"/>
+            </div>
+            <div className="ml-4">
+                <p className="text-base">Martin Miller</p>
+                <p className="text-sm">Our Agent</p>
+            </div>
+            </div>
+            <button className="btn w-50 font-normal">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" /></svg>
             Discover More
             </button>
